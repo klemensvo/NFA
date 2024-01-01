42e778f2b13fb55fe0edc7c09c1ef36b1cfc7f05
Klemens Vospernik <klemens.vospernik@gmx.at>
2024-01-01 15:11:02 +0100

    added transitions and acceptsWord, first logic test passes

--- src/main/java/ab1/impl/GRUPPE/NFAImpl.java
+++ src/main/java/ab1/impl/GRUPPE/NFAImpl.java
@@ -4,14 +4,15 @@ import ab1.FinalizedStateException;
 import ab1.NFA;
 import ab1.Transition;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class NFAImpl implements NFA {
     String initialState;
     Set<String> states = new HashSet<>();
     Set<String> acceptingStates = new HashSet<>();
+    final private Map<String, Set<Transition>> transitions = new HashMap<>();
+
+    boolean isFinalized = false;
 
     public NFAImpl(String initialState) {
         this.initialState = initialState;
@@ -24,7 +25,11 @@ public class NFAImpl implements NFA {
 
     @Override
     public Collection<Transition> getTransitions() {
-        return null;
+        Set<Transition> allTransitions = new HashSet<>();
+        for (Set<Transition> transitionSet : transitions.values()) {
+            allTransitions.addAll(transitionSet);
+        }
+        return allTransitions;
     }
 
     @Override
@@ -39,9 +44,17 @@ public class NFAImpl implements NFA {
 
     @Override
     public void addTransition(Transition transition) throws FinalizedStateException {
+        if (isFinalized) {
+            throw new FinalizedStateException();
+        }
         // add any state appearing in fromState or toState to states
         states.add(transition.fromState());
         states.add(transition.toState());
+        // add transitions from states
+        Set<Transition> stateTransitions =
+                transitions.computeIfAbsent(transition.fromState(),
+                        currentState -> new HashSet<>());
+        stateTransitions.add(transition);
     }
 
     @Override
@@ -82,21 +95,49 @@ public class NFAImpl implements NFA {
 
     @Override
     public boolean isFinalized() {
-        return false;
+        return isFinalized;
     }
 
     @Override
     public void finalizeAutomaton() {
-
+        isFinalized = true;
     }
 
     @Override
-    public boolean isFinite() {
+    public boolean isFinite() { // todo
         return false;
     }
 
     @Override
     public boolean acceptsWord(String word) {
-        return false;
+        boolean isAccepted = false;
+        char[] symbols = word.toCharArray();
+
+        // variable currentStates for all possible states of the
+        // non-deterministic automaton
+        Set<String> currentStates = new HashSet<>();
+        currentStates.add(initialState);
+
+        for (char symbol : symbols) {
+            Set<String> nextStates = new HashSet<>();
+            for (String state : currentStates) {
+                Set<Transition> stateTransitions =
+                        transitions.getOrDefault(state, Collections.emptySet());
+                for (Transition transition : stateTransitions) {
+                    if (transition.readSymbol() != null &&
+                            transition.readSymbol() == symbol) {
+                        nextStates.add(transition.toState());
+                    }
+                }
+            }
+            currentStates = nextStates;
+        }
+        for (String state : currentStates) {
+            if (acceptingStates.contains(state)) {
+                isAccepted = true;
+            }
+        }
+
+        return isAccepted;
     }
 }
--- src/test/java/ab1/tests/CustomTests.java
+++ src/test/java/ab1/tests/CustomTests.java
@@ -1,12 +1,13 @@
 package ab1.tests;
 
+import ab1.FinalizedStateException;
 import ab1.NFAFactory;
 import ab1.NFAProvider;
 import ab1.Transition;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CustomTests {
 
@@ -81,5 +82,156 @@ public class CustomTests {
 
         assertEquals("START", instance.getInitialState());
     }
+
+    @Test
+    public void finalizeTest1() {
+        var instance = factory.buildNFA("START");
+
+        assertFalse(instance.isFinalized());
+
+        instance.finalizeAutomaton();
+
+        assertTrue(instance.isFinalized());
+    }
+
+    @Test
+    public void finalizeTest2() {
+        var instance = factory.buildNFA("START");
+        instance.finalizeAutomaton();
+
+        assertThrows(
+                FinalizedStateException.class,
+                () -> instance.addTransition(
+                        Transition.builder()
+                                .fromState("START")
+                                .readSymbol('a')
+                                .toState("ACCEPT")
+                                .build()
+                )
+        );
+    }
+
+    @Test
+    public void languageTest1() {
+        var instance = factory.buildNFA("START");
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("START")
+                        .readSymbol('a')
+                        .toState("ACCEPT")
+                        .build()
+        );
+        instance.addAcceptingState("ACCEPT");
+        instance.finalizeAutomaton();
+
+        assertTrue(instance.acceptsWord("a"));
+        assertFalse(instance.acceptsWord("aa"));
+        assertFalse(instance.acceptsWord("ba"));
+        assertFalse(instance.acceptsWord("xyaz"));
+        assertFalse(instance.acceptsWord("ETI is fun!"));
+    }
+
+    /*
+    @Test
+    public void language2Test() {
+        var instance = factory.buildNFA("START");
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("START")
+                        .readSymbol('a')
+                        .toState("ACCEPT")
+                        .build()
+        );
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("ACCEPT")
+                        .readSymbol('a')
+                        .toState("ACCEPT")
+                        .build()
+        );
+        instance.addAcceptingState("ACCEPT");
+        instance.finalizeAutomaton();
+
+        assertTrue(instance.acceptsWord("a"));
+        assertTrue(instance.acceptsWord("aa"));
+        assertTrue(instance.acceptsWord("aaa"));
+        assertTrue(instance.acceptsWord("aaaa"));
+        assertTrue(instance.acceptsWord("aaaaaaaaaaaaaaaaaaaaaa"));
+        assertFalse(instance.acceptsWord("ba"));
+        assertFalse(instance.acceptsWord("xyaz"));
+        assertFalse(instance.acceptsWord("ETI is fun!"));
+    }
+
+    @Test
+    public void language3Test() {
+        var instance = factory.buildNFA("START");
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("START")
+                        .readSymbol(null)
+                        .toState("S1")
+                        .build()
+        );
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("START")
+                        .readSymbol(null)
+                        .toState("S2")
+                        .build()
+        );
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("S1")
+                        .readSymbol('a')
+                        .toState("ACCEPT")
+                        .build()
+        );
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("S2")
+                        .readSymbol('b')
+                        .toState("ACCEPT")
+                        .build()
+        );
+        instance.addAcceptingState("ACCEPT");
+        instance.finalizeAutomaton();
+
+        assertTrue(instance.acceptsWord("a"));
+        assertTrue(instance.acceptsWord("b"));
+        assertFalse(instance.acceptsWord("ba"));
+        assertFalse(instance.acceptsWord("xyaz"));
+        assertFalse(instance.acceptsWord("ETI is fun!"));
+    }
+
+    @Test
+    public void language4Test() {
+        var instance = factory.buildNFA("START");
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("START")
+                        .readSymbol('a')
+                        .toState("ACCEPT")
+                        .build()
+        );
+        instance.addTransition(
+                Transition.builder()
+                        .fromState("ACCEPT")
+                        .readSymbol(null)
+                        .toState("START")
+                        .build()
+        );
+        instance.addAcceptingState("ACCEPT");
+        instance.finalizeAutomaton();
+
+        assertTrue(instance.acceptsWord("a"));
+        assertTrue(instance.acceptsWord("aa"));
+        assertTrue(instance.acceptsWord("aaa"));
+        assertTrue(instance.acceptsWord("aaaa"));
+        assertTrue(instance.acceptsWord("aaaaaaaaaaaaaaaaaaaaaa"));
+        assertFalse(instance.acceptsWord("ba"));
+        assertFalse(instance.acceptsWord("xyaz"));
+        assertFalse(instance.acceptsWord("ETI is fun!"));
+    }
+     */
 
 }
